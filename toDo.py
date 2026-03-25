f51ed93de653d9b56695342f3f88bd0094fd4146
Hyemin Yang <asitwas729@gmail.com>
2026-03-25 19:28:11 +0900

    feat : task(CRUD) 구현

--- toDo.py
+++ toDo.py
@@ -1,8 +1,19 @@
+task = []
 # task 등록
-print(f"new task : ")
-add = input()
-task = ["task"]
-task.append(add)
-# 전체 task 출력
-for t in task:
-    print(f"{t}")
+while True:
+    print(f"1. 등록   2. 수정   3. 조회   4. 삭제")
+
+    sel = input()
+    if sel == "1":
+        new = input()
+        task.append(new)
+    elif sel == "2":
+        num = int(input())
+        task[num-1] = input()
+    elif sel == "3":
+        for i in task:
+            print(f"{i}")
+    elif sel == "4":
+        task.remove(task[int(input())-1])
+    elif sel.lower() == "q":
+        break
