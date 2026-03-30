8774f1c721f3be91aed3b113094a730ce7ee57fd
Hyemin Yang <asitwas729@gmail.com>
2026-03-30 17:51:33 +0900

    feat : Python 문법 연습 파이썬 문법, 자료구조, 함수, 클래스 기초 학습 내용

---
+++ python_basic_grammar.py
@@ -0,0 +1,29 @@
+user_name = "hyemin"
+user_age = 20
+if user_age < 20:
+    age_type = "minors"
+elif user_age >= 20:
+    age_type = "adults"
+else :
+    print("잘못 입력하셨습니다.")
+
+count = 0
+while count < 3:
+    print(count)
+    count += 1
+
+user = [1]
+user.append(4)
+print(user)
+
+def add(name, income):
+    if name == true :
+        return 1
+
+class Account:
+    def __init__(self, name, income):
+        self.name = name
+        self.balance = income
+    
+    def deposit(self, amount):
+        self.balance += amount
